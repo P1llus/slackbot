1b6bfb015e31c23d43144e4eda190a0edea8de24
marius <marius@devmachine.home>
2017-02-21 18:22:37 +0100

    tried to add helper file, will do it later

--- bin/Slackbot.php
+++ bin/Slackbot.php
@@ -18,9 +18,11 @@ use Mpociot\BotMan\BotManFactory;
 * Import the plugins
 */
 
+use Pillus\Slackbot\Modules\Shodan\Plugin as ShodanPlugin;
+use Pillus\Slackbot\Modules\Wpscan\Plugin as WpscanPlugin;
 use Pillus\Slackbot\Modules\Pastebin\Plugin as PastebinPlugin;
 use Pillus\Slackbot\Modules\Virustotal\Plugin as VirustotalPlugin;
-use Pillus\Slackbot\Modules\Shodan\Plugin as ShodanPlugin;
+
 
 /**
 * Load config file for the bot itself
@@ -45,6 +47,7 @@ $plugins = [
     (new PastebinPlugin)->setup($botman),
     (new VirusTotalPlugin)->setup($botman),
     (new ShodanPlugin)->setup($botman),
+    (new WpscanPlugin)->setup($botman),
 ];
 
 /**
@@ -54,6 +57,12 @@ $plugins = [
 foreach ($plugins as $plugin) {
     $plugin->init();
 }
+
+/**
+* Help Commands
+*/
+
+
 
 /**
 * Initiate the bot instance
